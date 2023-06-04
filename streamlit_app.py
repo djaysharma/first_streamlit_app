ebecfba5c59c63661f22d06f1eaac013e91a3665
djaysharma <djay_sharma@live.com>
2023-06-04 19:47:39 +0530

    added fruit choice for user to fetch data from api

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,10 @@ streamlit.dataframe(fruits_to_show)
 # new section for API calls
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
+fruit_choice = streamlit.text_input('What fruite would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
 #Flatten json response into a table
 fruityvice_response_normalised = pandas.json_normalize(fruityvice_response.json())
 #show the normalised table
