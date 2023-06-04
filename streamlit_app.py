c22ebac85fe028ab3b58ca990bccdd17632ad51f
djaysharma <djay_sharma@live.com>
2023-06-04 21:46:22 +0530

    added buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def get_fruityvice_data(the_fruit_choice):
     fruityvice_response_normalised = pandas.json_normalize(fruityvice_response.json())
     #show the normalised table
     return fruityvice_response_normalised
-    
+
 try:
     # new section for API calls
     streamlit.header("Fruityvice Fruit Advice!")
@@ -44,20 +44,31 @@ try:
 except URLError as e:
     streamlit.error()
 
-streamlit.stop()
 
-#Query Snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_row)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.text('Thanks for adding ' + add_my_fruit)
+#Snowflake related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        #Query Snowflake
+        my_cur.execute('select * from fruit_load_list')
+        return my_cur.fetchall()
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#A button to load the fruit
+if streamlit.button('Get fruit load list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
+#Allow a user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return 'Thanks for adding ' + new_fruit
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
+if streamlit.button('Add a Fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
